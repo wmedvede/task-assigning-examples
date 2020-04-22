660b11a74ee9a0cbc1b4111103d14c7042801a3e
Walter Medvedeo <wmedvedeo@gmail.com>
2020-04-22 16:36:44 +0200

    DB user system tables modification

--- kie-server-task-assigning-user-system-db-kjar/src/main/resources/tables.sql
+++ kie-server-task-assigning-user-system-db-kjar/src/main/resources/tables.sql
@@ -1,33 +1,21 @@
 -- postgres sql database structure for the DBUserSystemService implementation.
 
-CREATE TABLE public.ta_user
-(
-userid character varying(255) NOT NULL,
-enabled smallint NOT NULL,
-descritpion VARCHAR(255),
+CREATE TABLE public.ta_user (
+  userid character varying(255) NOT NULL,
+  enabled smallint NOT NULL,
+  descritpion VARCHAR(255),
 
-CONSTRAINT ta_user_pkey PRIMARY KEY (userid)
-)
-WITH(
-OIDS=FALSE
+  CONSTRAINT ta_user_pkey PRIMARY KEY (userid)
 );
 
-CREATE TABLE public.ta_user_group
-(
-userid character varying(255) NOT NULL,
-groupid character varying(255)
-)
-WITH(
-OIDS=FALSE
+CREATE TABLE public.ta_user_group (
+  userid character varying(255) NOT NULL,
+  groupid character varying(255)
 );
 
-CREATE TABLE public.ta_user_skill
-(
-userid character varying(255) NOT NULL,
-skillid character varying(255)
-)
-WITH(
-OIDS=FALSE
+CREATE TABLE public.ta_user_skill (
+  userid character varying(255) NOT NULL,
+  skillid character varying(255)
 );
 
 ALTER TABLE public.ta_user
